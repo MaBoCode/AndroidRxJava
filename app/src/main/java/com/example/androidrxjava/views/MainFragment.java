a77f3afca2ce4b9298b8e998a3527f71345eaa2d
Matthias Brown <matthias.brown@epita.fr>
2021-01-19 19:08:20 +0100

    Remove chart work

--- app/src/main/java/com/example/androidrxjava/views/MainFragment.java
+++ app/src/main/java/com/example/androidrxjava/views/MainFragment.java
@@ -7,27 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.androidrxjava.R;
 import com.example.androidrxjava.core.MainFragmentViewModel;
 import com.example.androidrxjava.core.MainFragmentViewModel.LoadingStatus;
 import com.example.androidrxjava.core.user.User;
 import com.example.androidrxjava.databinding.FrgMainBinding;
 import com.example.androidrxjava.injects.base.BaseFragment;
-import com.example.androidrxjava.utils.ThemeUtils;
 import com.example.androidrxjava.views.utils.AnimationUtils;
-import com.example.androidrxjava.views.utils.ChartFactory;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import org.androidannotations.annotations.EFragment;
 
@@ -44,21 +33,10 @@ public class MainFragment extends BaseFragment {
 
     protected MainFragmentViewModel viewModel;
 
-    protected LineChart lineChart;
-
-    protected View.OnClickListener onRefreshClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            refreshChartData();
-        }
-    };
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FrgMainBinding.inflate(inflater, container, false);
-
-        binding.btnRefreshData.setOnClickListener(onRefreshClick);
 
         viewModel = new ViewModelProvider(requireActivity()).get(MainFragmentViewModel.class);
 
@@ -87,10 +65,6 @@ public class MainFragment extends BaseFragment {
             public void onChanged(List<User> users) {
                 String usersCount = String.format("%s users", users.size());
                 binding.usersCountTxtView.setText(usersCount);
-
-                binding.btnRefreshData.setVisibility(View.VISIBLE);
-
-                refreshChartData();
             }
         });
 
@@ -101,76 +75,6 @@ public class MainFragment extends BaseFragment {
                 binding.loader.setVisibility(visiblity);
             }
         });
-    }
-
-    public void initChart() {
-        ChartFactory chartFactory = new ChartFactory();
-        lineChart = chartFactory.getLineChart(requireContext());
-
-        ConstraintLayout parentLayout = (ConstraintLayout) binding.getRoot();
-        lineChart.setId(parentLayout.getChildCount());
-
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(0, 0);
-        layoutParams.setMargins(0, 0, 0, 32);
-        parentLayout.addView(lineChart, parentLayout.getChildCount() - 2, layoutParams);
-
-        ConstraintSet constraintSet = new ConstraintSet();
-        constraintSet.clone(parentLayout);
-
-        constraintSet.connect(R.id.usersCountTxtView, ConstraintSet.BOTTOM, lineChart.getId(), ConstraintSet.TOP);
-
-        constraintSet.connect(lineChart.getId(), ConstraintSet.TOP, R.id.usersCountTxtView, ConstraintSet.BOTTOM);
-        constraintSet.connect(lineChart.getId(), ConstraintSet.START, R.id.leftGuideline, ConstraintSet.START);
-        constraintSet.connect(lineChart.getId(), ConstraintSet.END, R.id.rightGuideline, ConstraintSet.END);
-        constraintSet.connect(lineChart.getId(), ConstraintSet.BOTTOM, R.id.btnRefreshData, ConstraintSet.TOP);
-
-        constraintSet.connect(R.id.btnRefreshData, ConstraintSet.TOP, lineChart.getId(), ConstraintSet.BOTTOM);
-
-        constraintSet.applyTo(parentLayout);
-    }
-
-    public void refreshChartData() {
-
-        if (lineChart == null) {
-            initChart();
-        }
-
-        LineData lineData = lineChart.getData();
-
-        if (lineData == null) {
-            lineData = new LineData();
-            lineData.setDrawValues(false);
-            lineChart.setData(lineData);
-        }
-
-        ILineDataSet set = lineData.getDataSetByIndex(0);
-
-        if (set == null) {
-            set = createSet();
-            lineData.addDataSet(set);
-        }
-
-        int randomDataSetIndex = (int) (Math.random() * lineData.getDataSetCount());
-        ILineDataSet randomSet = lineData.getDataSetByIndex(randomDataSetIndex);
-        float value = (float) (Math.random() * 50) + 50f * (randomDataSetIndex + 1);
-
-        lineData.addEntry(new Entry(randomSet.getEntryCount(), value), randomDataSetIndex);
-        lineData.notifyDataChanged();
-
-        lineChart.notifyDataSetChanged();
-
-        lineChart.setVisibleXRangeMaximum(6);
-        lineChart.moveViewTo(lineData.getEntryCount() - 7, 50f, YAxis.AxisDependency.LEFT);
-    }
-
-    public LineDataSet createSet() {
-        LineDataSet lineDataSet = new LineDataSet(null, "Dataset 1");
-        lineDataSet.setDrawValues(false);
-        lineDataSet.setDrawCircles(false);
-        lineDataSet.setLineWidth(2f);
-        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        lineDataSet.setColor(ThemeUtils.getThemeColor(requireContext(), R.attr.colorPrimary));
-        return lineDataSet;
     }
 
     public void startAnimations() {
